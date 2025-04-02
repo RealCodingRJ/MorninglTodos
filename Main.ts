7f1809008a16daacfe0aa1089bb7b41c72387d34
Ryan James <realcoder2023@gmail.com>
2025-04-02 17:07:54 -0400

    fixed the coreectness of code

--- Main.ts
+++ Main.ts
@@ -1,26 +1,24 @@
 import { TodoTyped } from "./Todos/TodosTypes";
 import { isCompleted } from "./Todos/TodosTypes";
 
-let todos: TodoTyped | Array<string> = [];
+let todosOne: isCompleted = {
+  isCompleted: true,
+};
 
-todos.push("Pill") as unknown as isCompleted;
-todos.push("Coffee And Food") as unknown as isCompleted;
-todos.push("Shower / Brush Teeth") as unknown as isCompleted;
-todos.push("Lunch") as unknown as isCompleted;
-todos.push("Laundry") as unknown as isCompleted;
-todos.push("Clean Room") as unknown as isCompleted;
-if (!isCompleteTodos(todos.toString())) {
+const todos: string[] = [];
+
+todos.push("Pill");
+todos.push("Coffee And Food");
+todos.push("Shower / Brush Teeth");
+todos.push("Lunch");
+todos.push("Laundry");
+todos.push("Clean Room");
+
+if (!todosOne.isCompleted) {
   console.log("Not Completed Not Computer");
 } else {
-  for (let todo in todos) {
-    console.log("Step " + todo + " " + todos[todo].toUpperCase());
+  todos.forEach((todo) => {
+    console.log("Step " + todo.toUpperCase());
     console.log();
-  }
-}
-function getArgsTodo(args: any) {
-  return args;
-}
-
-function isCompleteTodos(a: string, isCompleted?: isCompleted) {
-  return isCompleted ? false : true;
+  });
 }
